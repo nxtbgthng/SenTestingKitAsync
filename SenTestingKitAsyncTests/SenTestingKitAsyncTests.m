8e67bab95038224442dde0e512d4aa1c6cb1f65d
Tobias Kräntzer <info@tobias-kraentzer.de>
2013-01-07 22:10:29 +0100

    Add documentation and extend examples.

--- SenTestingKitAsyncTests/SenTestingKitAsyncTests.m
+++ SenTestingKitAsyncTests/SenTestingKitAsyncTests.m
@@ -22,14 +22,34 @@
     int64_t delayInSeconds = 2.0;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-//        STFail(@"But if your test methods ends with 'Async', you can do this!");
+        STFail(@"But if your test methods ends with 'Async', you can do this!");
         STSuccess();
     });
 }
 
-//- (void)testAfterAsync;
-//{
-//    STFailAfter(5, @"Timeout!");
-//}
+- (void)testAfterAsync;
+{
+    STFailAfter(1, @"Timeout!");
+}
+
+- (void)testSelectorAsync;
+{
+    [self performSelector:@selector(mySelector) withObject:nil afterDelay:2.0];
+}
+
+- (void)mySelector;
+{
+    STSuccess();
+}
+
+- (void)testOtherSelectorAsync;
+{
+    [self performSelector:@selector(myOtherSelector:) withObject:@(2) afterDelay:2.0];
+}
+
+- (void)myOtherSelector:(NSNumber *)value;
+{
+    STAssertEqualObjects(value, @(3), @"Expecting the number '3'.");
+}
 
 @end
